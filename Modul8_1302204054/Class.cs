6c5c3d470d62bcb3cfa85620104e916165617bee
Aditya MP <211wadityaw112@gmail.com>
2022-04-23 14:18:05 +0700

    Add project files.

---
+++ Modul8_1302204054/Class.cs
@@ -0,0 +1,20 @@
+﻿namespace Modul8_1302204054
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public List<string> stars { get; set; }
+        public string description { get; set; }
+
+        public Movie(string title, string director, List<string> stars, string description)
+        {
+            this.title = title;
+            this.director = director;
+            this.stars = stars;
+            this.description = description;
+        }
+
+
+    }
+}
